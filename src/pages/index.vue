f0546f2795cf487524252a88690a1d9600d7d266
Oscar Bahamonde <107950590+obahamonde@users.noreply.github.com>
2022-06-30 02:47:41 -0500

    'm'

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,16 @@
 <template>
-<div col center>
-Sign in with AWS Cognito
-<CognitoAuth dark:invert />
-</div>
+  <div col center>
+    Sign in with AWS Cognito
+    <CognitoAuth dark:invert />
+    <p>{{ data }}</p>
+  </div>
 </template>
+<script setup lang="ts">
+const data = ref();
+async function fetchData() {
+  const res = await fetch("/api/");
+  const json = await res.json();
+  data.value = json;
+}
+onMounted(fetchData());
+</script>
